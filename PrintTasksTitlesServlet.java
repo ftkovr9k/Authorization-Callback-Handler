c9eaa38cc072a032e7f05f333281bc812d92365d
Farhan Tanvir Khan <90358727+ftkovr9k@users.noreply.github.com>
2021-09-29 20:16:41 -0400

    Update PrintTasksTitlesServlet.java

--- PrintTasksTitlesServlet.java
+++ PrintTasksTitlesServlet.java
@@ -1,28 +1,62 @@
 package com.google.oauthsample;
 
-import com.google.api.client.auth.oauth2.draft10.AccessTokenResponse;
+import ...
 
 /**
- * Allows easy storage and access of authorization tokens.
+ * Simple sample Servlet which will display the tasks in the default task list of the user.
  */
-public interface OAuthTokenDao {
+@SuppressWarnings("serial")
+public class PrintTasksTitlesServlet extends HttpServlet {
 
   /**
-   * Stores the given AccessTokenResponse using the {@code username}, the OAuth
-   * {@code clientID} and the tokens scopes as keys.
-   *
-   * @param tokens The AccessTokenResponse to store
-   * @param userName The userName associated wit the token
+   * The OAuth Token DAO implementation, used to persist the OAuth refresh token.
+   * Consider injecting it instead of using a static initialization. Also we are
+   * using a simple memory implementation as a mock. Change the implementation to
+   * using your database system.
    */
-  public void saveKeys(AccessTokenResponse tokens, String userName);
+  public static OAuthTokenDao oauthTokenDao = new OAuthTokenDaoMemoryImpl();
+
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    // Getting the current user
+    // This is using App Engine's User Service but you should replace this to
+    // your own user/login implementation
+    UserService userService = UserServiceFactory.getUserService();
+    User user = userService.getCurrentUser();
+
+    // If the user is not logged-in it is redirected to the login service, then back to this page
+    if (user == null) {
+      resp.sendRedirect(userService.createLoginURL(getFullRequestUrl(req)));
+      return;
+    }
+
+    // Checking if we already have tokens for this user in store
+    AccessTokenResponse accessTokenResponse = oauthTokenDao.getKeys(user.getEmail());
+
+    // If we don't have tokens for this user
+    if (accessTokenResponse == null) {
+      OAuthProperties oauthProperties = new OAuthProperties();
+      // Redirect to the Google OAuth 2.0 authorization endpoint
+      resp.sendRedirect(new GoogleAuthorizationRequestUrl(oauthProperties.getClientId(),
+          OAuthCodeCallbackHandlerServlet.getOAuthCodeCallbackHandlerUrl(req), oauthProperties
+              .getScopesAsString()).build());
+      return;
+    }
+  }
 
   /**
-   * Returns the AccessTokenResponse stored for the given username, clientId and
-   * scopes. Returns {@code null} if there is no AccessTokenResponse for this
-   * user and scopes.
+   * Construct the request's URL without the parameter part.
    *
-   * @param userName The username of which to get the stored AccessTokenResponse
-   * @return The AccessTokenResponse of the given username
+   * @param req the HttpRequest object
+   * @return The constructed request's URL
    */
-  public AccessTokenResponse getKeys(String userName);
+  public static String getFullRequestUrl(HttpServletRequest req) {
+    String scheme = req.getScheme() + "://";
+    String serverName = req.getServerName();
+    String serverPort = (req.getServerPort() == 80) ? "" : ":" + req.getServerPort();
+    String contextPath = req.getContextPath();
+    String servletPath = req.getServletPath();
+    String pathInfo = (req.getPathInfo() == null) ? "" : req.getPathInfo();
+    String queryString = (req.getQueryString() == null) ? "" : "?" + req.getQueryString();
+    return scheme + serverName + serverPort + contextPath + servletPath + pathInfo + queryString;
+  }
 }
