0189c088bea2098b819506d73350920240c265b9
Farhan Tanvir Khan <90358727+ftkovr9k@users.noreply.github.com>
2021-09-29 20:26:50 -0400

    Update OAuthTokenDaoMemoryImpl.java

--- OAuthTokenDaoMemoryImpl.java
+++ OAuthTokenDaoMemoryImpl.java
@@ -1,22 +1,85 @@
 package com.google.oauthsample;
 
-import com.google.api.client.auth.oauth2.draft10.AccessTokenResponse;
-...
+import ...
 
 /**
- * Quick and Dirty memory implementation of {@link OAuthTokenDao} based on
- * HashMaps.
+ * Object representation of an OAuth properties file.
  */
-public class OAuthTokenDaoMemoryImpl implements OAuthTokenDao {
+public class OAuthProperties {
 
-  /** Object where all the Tokens will be stored */
-  private static Map tokenPersistance = new HashMap();
+  public static final String DEFAULT_OAUTH_PROPERTIES_FILE_NAME = "oauth.properties";
 
-  public void saveKeys(AccessTokenResponse tokens, String userName) {
-    tokenPersistance.put(userName, tokens);
+  /** The OAuth 2.0 Client ID */
+  private String clientId;
+
+  /** The OAuth 2.0 Client Secret */
+  private String clientSecret;
+
+  /** The Google APIs scopes to access */
+  private String scopes;
+
+  /**
+   * Instantiates a new OauthProperties object reading its values from the
+   * {@code OAUTH_PROPERTIES_FILE_NAME} properties file.
+   *
+   * @throws IOException IF there is an issue reading the {@code propertiesFile}
+   * @throws OauthPropertiesFormatException If the given {@code propertiesFile}
+   *           is not of the right format (does not contains the keys {@code
+   *           clientId}, {@code clientSecret} and {@code scopes})
+   */
+  public OAuthProperties() throws IOException {
+    this(OAuthProperties.class.getResourceAsStream(DEFAULT_OAUTH_PROPERTIES_FILE_NAME));
   }
 
-  public AccessTokenResponse getKeys(String userName) {
-    return tokenPersistance.get(userName);
+  /**
+   * Instantiates a new OauthProperties object reading its values from the given
+   * properties file.
+   *
+   * @param propertiesFile the InputStream to read an OAuth Properties file. The
+   *          file should contain the keys {@code clientId}, {@code
+   *          clientSecret} and {@code scopes}
+   * @throws IOException IF there is an issue reading the {@code propertiesFile}
+   * @throws OAuthPropertiesFormatException If the given {@code propertiesFile}
+   *           is not of the right format (does not contains the keys {@code
+   *           clientId}, {@code clientSecret} and {@code scopes})
+   */
+  public OAuthProperties(InputStream propertiesFile) throws IOException {
+    Properties oauthProperties = new Properties();
+    oauthProperties.load(propertiesFile);
+    clientId = oauthProperties.getProperty("clientId");
+    clientSecret = oauthProperties.getProperty("clientSecret");
+    scopes = oauthProperties.getProperty("scopes");
+    if ((clientId == null) || (clientSecret == null) || (scopes == null)) {
+      throw new OAuthPropertiesFormatException();
+    }
+  }
+
+  /**
+   * @return the clientId
+   */
+  public String getClientId() {
+    return clientId;
+  }
+
+  /**
+   * @return the clientSecret
+   */
+  public String getClientSecret() {
+    return clientSecret;
+  }
+
+  /**
+   * @return the scopes
+   */
+  public String getScopesAsString() {
+    return scopes;
+  }
+
+  /**
+   * Thrown when the OAuth properties file was not at the right format, i.e not
+   * having the right properties names.
+   */
+  @SuppressWarnings("serial")
+  public class OAuthPropertiesFormatException extends RuntimeException {
   }
 }
